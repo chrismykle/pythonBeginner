c4ec13fea17df8e5bee4d00e403c678393e5512f
chrismykle <71075178+chrismykle@users.noreply.github.com>
2022-03-18 13:08:31 +0100

    Adding workingWithNumbers.py and inputFromUsers.py

--- workingWithNumbers.py
+++ workingWithNumbers.py
@@ -1,2 +1,18 @@
+from math import *
 
-print(10 & 3)  # Reads 10 mod 3, means 10 divided by 3, bur print the "rest". 3+3+3 = 9, 
+print(10 & 3)  # Reads 10 mod 3, means 10 divided by 3, bur print the "rest". 3+3+3 = 9,
+my_num = -5
+print(my_num)
+print(str(my_num) + " my favorite number")  # For printing strings and numbers in the same print statement
+print(abs(my_num))   # Absoluttverdien
+print(pow(3, 2))     # 3^2
+print(max(4, 6))     # Printing the biggest number
+print(min(4, 6))     # Printing the smallest number
+print(round(3.7))    # Rounds off a number
+
+# Imported
+print("Importere funksjoner:")
+
+print(floor(2.8))    # Floor value
+print(ceil(2.3))     # Ceiling value
+print(sqrt(25))      # Square root
