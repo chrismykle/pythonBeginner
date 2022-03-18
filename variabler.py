9b0d9be39cd9742aa0972df63edfe6d247db0334
chrismykle <71075178+chrismykle@users.noreply.github.com>
2022-03-18 09:32:41 +0100

    added 2 files

---
+++ variabler.py
@@ -0,0 +1,9 @@
+
+character_name = "Tom"
+character_age = "59"
+character_gender = "male"
+
+print("There once was a " + character_gender + " named " + character_name + ",")
+print("He was " + character_age + " years old.")
+print("He liked the name " + character_name + ",")
+print("But did not like being " + character_age + " years old.")
---
+++ workingWithStrings.py
@@ -0,0 +1,7 @@
+phrase = "Dette er en frase"
+print(phrase.upper().isupper())
+print(phrase.replace("en", "ei"))
+print(phrase[4])
+print(phrase.index("er"))
+print(len(phrase))
+print(phrase)
