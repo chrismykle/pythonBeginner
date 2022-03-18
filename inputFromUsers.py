20143e9d4b2d5f8e699e40715fbefc405a9203be
chrismykle <71075178+chrismykle@users.noreply.github.com>
2022-03-18 13:21:42 +0100

    Adding basicCalculator.py and inputFromUsers.py and madLibsGame.py

--- inputFromUsers.py
+++ inputFromUsers.py
@@ -1,5 +1,5 @@
 
-name = input("Enter your name: ")# The variable name get the value of input
+name = input("Enter your name: ")   # The variable name get the value of input
 age = input("Enter your age: ")
 gender = input("Enter your gender: ")
 print("Hello " + name + "! You are " + age + " and you are a " + gender)
