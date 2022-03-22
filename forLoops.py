9b727a8dff6a6151c67ff703c2bcfa4895268da8
chrismykle <71075178+chrismykle@users.noreply.github.com>
2022-03-22 09:50:46 +0100

    Added and worked on exponentFunction.py and forLoops.py

---
+++ forLoops.py
@@ -0,0 +1,15 @@
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+print()
+for index in range(7):
+    print(index)
+
+print()
+venner = ["Jon", "Per", "Pring", "Simen", "Ole"]
+for nummer in range(len(venner)):
+    if nummer == 4:
+        print("Hei Ole!")
+    else:
+        print(venner[nummer])
