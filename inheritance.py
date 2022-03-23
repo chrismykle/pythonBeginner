d103e351d07df0823345daf0cb86f2d5c8b47e27
chrismykle <71075178+chrismykle@users.noreply.github.com>
2022-03-23 16:17:24 +0100

    Added and worked on inheritance.py, inheritanceChef.py and inheritanceChineseChef.py
    Inheritance can be used to have one class inherit another class's function

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+from inheritanceChef import Chef
+from inheritanceChineseChef import ChineseChef
+
+
+myChef = Chef()
+myChef.make_chikcen()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chikcen()
+myChineseChef.make_special_dish()
